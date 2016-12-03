3cd3296d55c140ea40b220822f90542b0b800562
Matt Miller <matt.miller@brightbytes.net>
2016-12-02 17:05:17 -0700

    Adding the ability to embed subsections of a document that has a #hash in the url. Uses fancy overflow clipping trickery.

--- run.js
+++ run.js
@@ -1,32 +1,60 @@
 ;(function () {
 	var CONTENT_SELECTOR = 'article.entry-content';
-	var LINK_SELECTOR = 'a[href^="https://github.com"][href$=".md"], a[href^="/"][href$=".md"]';
 	var EMBED_STYLE = {
 		borderColor: '#4078c0',
-		background: '#fcfcff'
+		background: '#fcfcff',
+		position: 'relative'
 	};
+	var HREF_REGEX = /^((https?\:\/\/github\.com)|\/).*\.md(#.*)?$/;
 
-	var anchors = document.querySelector(CONTENT_SELECTOR).querySelectorAll(LINK_SELECTOR);
-	Array.prototype.forEach.call(anchors, function (anchor) {
+	var mainContent = document.querySelector(CONTENT_SELECTOR);
+	if (!mainContent) {
+		return;
+	}
+
+	var anchors = Array.prototype.slice.call(mainContent.querySelectorAll('a[href]'));
+	anchors = anchors.filter(function (anchor) {
+		var href = anchor.getAttribute('href');
+		if (HREF_REGEX.test(href)) {
+			anchor.setAttribute('data-jump', href.split('#')[1] || '');
+			return true;
+		}
+		return false;
+	});
+
+	anchors.forEach(function (anchor) {
 		getUrl(anchor.getAttribute('href'), function (err, html) {
 			if (err) {
 				throw err;
 			}
 
 			var doc = parseHtml(html);
-			var content = doc.querySelector(CONTENT_SELECTOR);
+			var content = parseHtml(doc.querySelector(CONTENT_SELECTOR).outerHTML).children[0];
+			content.innerHTML = '<div>' + content.innerHTML + '</div>';
+
+			var jump = anchor.getAttribute('data-jump');
+			if (jump) {
+				window.setTimeout(function () {
+					clipToRelevantContent(content, jump);
+				}, 100);
+				window.addEventListener('resize', function () {
+					clipToRelevantContent(content, jump);
+				});
+			}
 			Object.keys(EMBED_STYLE).forEach(function (prop) {
 				content.style[prop] = EMBED_STYLE[prop];
 			});
 
 			var close = document.createElement('a');
 			close.innerHTML = '<svg class="octicon octicon-x" viewBox="0 0 12 16" version="1.1" width="16" height="16"><path fill-rule="evenodd" d="M7.48 8l3.75 3.75-1.48 1.48L6 9.48l-3.75 3.75-1.48-1.48L4.52 8 .77 4.25l1.48-1.48L6 6.52l3.75-3.75 1.48 1.48z"></path></svg>';
-			close.className = 'float-right';
+			close.style.position = 'absolute';
+			close.style.top = '4px';
+			close.style.right = '8px';
 			close.style.cursor = 'pointer';
 			close.addEventListener('click', function () {
 				content.parentNode.removeChild(content);
 			});
-			content.insertBefore(close, content.firstChild);
+			content.firstChild.insertBefore(close, content.firstChild.firstChild);
 
 			var anchorBlock = getClosestBlockAncestor(anchor);
 			anchorBlock.insertAdjacentElement('afterend', content);
@@ -66,5 +94,58 @@
 		}
 
 		return null;
+	}
+
+	function clipToRelevantContent (content, sectionId) {
+		var sectionHeader = content.querySelector('#user-content-' + sectionId);
+		if (!sectionHeader) {
+			return;
+		}
+		sectionHeader = sectionHeader.parentNode;
+
+		content.style.overflowY = '';
+		content.style.height = '';
+		content.firstChild.style.marginTop = '';
+
+		var clone = content.cloneNode(true);
+		var sectionHeader = clone.querySelector('#user-content-' + sectionId).parentNode;
+
+		var tempContainer = document.createElement('div');
+		tempContainer.className = 'container readme';
+		tempContainer.style.position = 'absolute';
+		tempContainer.style.top = '0';
+		//tempContainer.style.opacity = '0';
+		var innerTempContainer = document.createElement('article');
+		innerTempContainer.className = 'markdown-body entry-content';
+		tempContainer.appendChild(innerTempContainer);
+		innerTempContainer.appendChild(clone);
+		document.body.appendChild(tempContainer);
+
+		var largerHeaderTags = [];
+		for (var i = parseInt(sectionHeader.tagName.charAt(1), 10); i >= 1; --i) {
+			largerHeaderTags.push('h' + i);
+		}
+
+		var nextHeader;
+		var allHeaders = clone.querySelectorAll(largerHeaderTags.join(', '));
+		Array.prototype.every.call(allHeaders, function (header, i) {
+			if (header === sectionHeader) {
+				nextHeader = allHeaders[i + 1];
+				return false;
+			}
+			return true;
+		});
+
+		var marginTop = '-' + (sectionHeader.offsetTop - sectionHeader.offsetHeight) + 'px';
+		var height = (
+			(nextHeader ? nextHeader.offsetTop : tempContainer.offsetHeight)
+			- sectionHeader.offsetTop
+		) + 'px';
+
+		document.body.removeChild(tempContainer);
+
+		content.style.overflowY = 'hidden';
+		content.style.height = height;
+		content.firstChild.style.setProperty('margin-top', marginTop, 'important');
 	}
 }());
